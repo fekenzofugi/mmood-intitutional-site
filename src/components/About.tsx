7015fd85bf9735def99db9c1c2ad2669d5b40a2e
Fernando Kenzo Imami Fugihara <f205067@dac.unicamp.br>
2026-03-20 15:12:04 -0300

    remove warnings

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,11 +2,34 @@ import styled from "styled-components"
 
 const About = () => {
     return (
-        <div>
-
-        </div>  
+        <Wrapper>
+            <h2>Sobre a MarketMood</h2>
+        </Wrapper>  
     )
 }
+
+const Wrapper = styled.section`
+    width: 100%;
+    padding: 4rem 2rem;
+    background: #f9f9f9;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    text-align: center;
+
+    h2 {
+        font-size: 2.5rem;
+        margin-bottom: 1rem;
+        color: var(--primary-color);
+    }
+
+    p {
+        font-size: 1.125rem;
+        max-width: 600px;
+        color: #555;
+        line-height: 1.6;
+    }
+`
 
 export default About
 
--- src/components/CanaisDeVenda.tsx
+++ src/components/CanaisDeVenda.tsx
@@ -2,11 +2,34 @@ import styled from "styled-components";
 
 const CanaisDeVenda = () => {
     return (
-        <>
-            
-        </>
+        <Wrapper>
+            <h2>Canais de Venda</h2>
+        </Wrapper>
     )
 }
+
+const Wrapper = styled.section`
+    width: 100%;
+    padding: 4rem 2rem;
+    background: #fff;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    text-align: center;
+
+    h2 {
+        font-size: 2.5rem;
+        margin-bottom: 1rem;
+        color: var(--primary-color);
+    }
+
+    p {
+        font-size: 1.125rem;
+        max-width: 600px;
+        color: #555;
+        line-height: 1.6;
+    }
+`
 
 
 export default CanaisDeVenda
